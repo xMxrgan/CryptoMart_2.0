f142c9a01b6629564ceafe10f702c94701a28c5d
Morgan <104979421+xMxrgan@users.noreply.github.com>
2025-04-02 10:46:55 +0200

    Starting API

---
+++ public/frontend/api/APIproducts.ts
@@ -0,0 +1,15 @@
+/*
+const url = 'https://api.themoviedb.org/3/authentication';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjN2E4YzYyMWRkZGM2YjBkMjIyZGI5NGZmMjcxMzBiZiIsIm5iZiI6MTc0Mjc2ODU2NS41LCJzdWIiOiI2N2UwODliNWY2YTBmY2JkMDA0ZGMxMDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9f_ejb0fF0ezYNLZZQq954yNvObEQcJ_zrGQg1GJJT4'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
+*/
